ecbae5ecdec87f943992268595617e8bb5709911
Luke Duncalfe <lduncalfe@eml.cc>
2018-04-30 14:38:40 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# nano-stream-core
+
+This library is a tiny and lightweight node server that streams block data from a [nano currency](https://nano.org/) node. It works by receiving blocks processed by the node via the [RPC callback](https://github.com/nanocurrency/raiblocks/wiki/RPC-protocol#rpc-callback), and streams that data to a socket that other processes can listen to.
+
+This package is intended to support other npm packages rather than to be run on its own.
+
+See:
+
+* nano-stream-ws - Stream to websockets
+* nano-stream-mqtt - Stream to mqtt (TODO)
+* nano-stream-ampq - Stream to ampq (TODO)
+
+Also read `How to write your own nano-stream plugin` (TODO)
+
+## Installation
+
+    npm install --global nano-stream-core
+
+## Usage
+
+### Start the streaming web server
+
+    node app
+
+The server will default to running on `http://127.0.0.1:3000`.
+
+Override these defaults by passing in `host` or `port` arguments:
+
+    node app host=http://ipv6-localhost port=3001
+
+
+## TODO
+
+Edit the RPC config.
---
+++ app.js
@@ -0,0 +1,83 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+var ipc = require('node-ipc');
+
+// ipc configuration
+// TODO expose more ipc.config https://www.npmjs.com/package/node-ipc
+ipc.config.id = 'nanoStream';
+ipc.config.retry = 1500;
+
+
+// Default port and host of the webserver that receives callbacks from the
+// nano RPC
+let port = 3000;
+let host = 'http://127.0.0.1';
+
+
+// Process any args passed in and overwrite defaults
+const args =  process.argv.slice(2);
+args.forEach((arg, index, array) => {
+  const [key, value] = arg.split('=')
+  if (key === 'port') port = value;
+  if (key === 'host') host = value;
+});
+
+// Establish new ipc socket server
+ipc.serve(
+  function(){
+    ipc.server.on(
+      'connect',
+      function(socket) {
+        console.log('Client has connected!')
+      }
+    );
+    ipc.server.on(
+      'socket.disconnected',
+      function(socket) {
+        console.log('Client has disconnected!');
+      }
+    );
+  }
+);
+
+ipc.server.start();
+
+
+// Webserver request handler
+const requestHandler = (request, response) => {
+  console.log(request.method, request.url)
+
+  // Handle any POST request
+  if (request.method === 'POST') {
+    let body = '';
+
+    request.on('data', chunk => body += chunk.toString());
+
+    // TODO make this a proper stream
+    request.on('end', () => {
+      payload = JSON.stringify(JSON.parse(body));
+
+      // Broadcast payload to all connected clients
+      if (ipc.server.sockets.length > 0) {
+        ipc.server.broadcast('payload', payload);
+      } else {
+        console.log(`No connected clients. Payload was: ${payload}`);
+      }
+
+      response.end('ok');
+    });
+  }
+
+}
+
+const webServer = http.createServer(requestHandler)
+
+webServer.listen(port, (err) => {
+  if (err) {
+    return console.log('Something bad happened', err)
+  }
+
+  console.log(`Web server is listening on ${host}:${port}`)
+})
