6eb0d3f0f577bd839788051bf6723096376965db
Luke Duncalfe <lduncalfe@eml.cc>
2018-04-30 23:05:41 +1200

    Remove unused args

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let host = 'http://127.0.0.1';
 
 // Process any args passed in and overwrite defaults
 const args =  process.argv.slice(2);
-args.forEach((arg, index, array) => {
+args.forEach((arg) => {
   const [key, value] = arg.split('=');
   if (key === 'port') port = value;
   if (key === 'host') host = value;
